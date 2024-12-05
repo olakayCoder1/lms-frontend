757f7d49bc66ca14c3790e8f5219cc0daf471492
olakayCoder1 <programmerolakay@gmail.com>
2024-12-05 16:08:10 +0100

    restructured the menu layout

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -524,7 +524,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
               <li>
               <button 
                 onClick={deleteUserFromLocalStorage}
-                className="flex items-center gap-3.5 px-6 py-4 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base">
+                className="flex items-center gap-3.5 px-6 py-4 text-sm font-medium duration-300 ease-in-out hover:text-red-700 lg:text-base">
                 <svg
                   className="fill-current"
                   width="22"
