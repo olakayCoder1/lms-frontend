56f3b0bb97f9f9fe5d8d446672ecf334fa4024de
olakayCoder1 <programmerolakay@gmail.com>
2025-01-08 12:47:14 +0100

    `Removed unnecessary div wrapper around Instructor Dropdown`

--- src/pages/Form/NewCourseForm.tsx
+++ src/pages/Form/NewCourseForm.tsx
@@ -128,7 +128,7 @@ const NewCourseForm = () => {
               </div>
 
               {/* Instructor Dropdown */}
-              <div className="mb-4.5">
+              {/* <div className="mb-4.5">
                 <label className="mb-2.5 block text-black dark:text-white">Instructor</label>
                 <div className="relative z-20 bg-transparent dark:bg-form-input">
                   <select
@@ -146,7 +146,7 @@ const NewCourseForm = () => {
                     ))}
                   </select>
                 </div>
-              </div>
+              </div> */}
 
               {/* Buttons */}
               <div className="flex justify-end gap-4.5">
