18bf371dc1c4437b0fbeb14cf3070131081ff487
olakayCoder1 <programmerolakay@gmail.com>
2025-04-27 10:38:56 +0100

    `Added course title column to StudentMaterialTable and TutorMaterialsTable components`

--- src/components/Tables/StudentMaterialTable.tsx
+++ src/components/Tables/StudentMaterialTable.tsx
@@ -316,6 +316,9 @@ const StudentMaterialTable = () => {
                   Title
                 </th>
                 <th className="min-w-[150px] py-4 px-4 font-medium text-black dark:text-white">
+                  Course
+                </th>
+                <th className="min-w-[150px] py-4 px-4 font-medium text-black dark:text-white">
                   Added date
                 </th>
                 <th className="py-4 px-4 font-medium text-black dark:text-white">
@@ -340,13 +343,17 @@ const StudentMaterialTable = () => {
                     </td>
                     <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
                       <p className="text-black dark:text-white">
+                        {packageItem?.course?.title}
+                      </p>
+                    </td>
+                    <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                      <p className="text-black dark:text-white">
                         {formatDate(packageItem.created_at)}
                       </p>
                     </td>
                     <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
                       <div className="flex items-center space-x-3.5">
-                        {/* Open modal to view document */}
-                        <button
+                        {/* <button
                           onClick={() => openModal(packageItem?.file, packageItem?.id)} // Pass correct URI
                           className="hover:text-primary"
                         >
@@ -361,7 +368,7 @@ const StudentMaterialTable = () => {
                             <path d="M8.99981 14.8219C3.43106 14.8219 0.674805 9.50624 0.562305 9.28124C0.47793 9.11249 0.47793 8.88749 0.562305 8.71874C0.674805 8.49374 3.43106 3.20624 8.99981 3.20624C14.5686 3.20624 17.3248 8.49374 17.4373 8.71874C17.5217 8.88749 17.5217 9.11249 17.4373 9.28124C17.3248 9.50624 14.5686 14.8219 8.99981 14.8219ZM1.85605 8.99999C2.4748 10.0406 4.89356 13.5562 8.99981 13.5562C13.1061 13.5562 15.5248 10.0406 16.1436 8.99999C15.5248 7.95936 13.1061 4.44374 8.99981 4.44374C4.89356 4.44374 2.4748 7.95936 1.85605 8.99999Z" />
                             <path d="M9 11.3906C7.67812 11.3906 6.60938 10.3219 6.60938 9C6.60938 7.67813 7.67812 6.60938 9 6.60938C10.3219 6.60938 11.3906 7.67813 11.3906 9C11.3906 10.3219 10.3219 11.3906 9 11.3906ZM9 7.875C8.38125 7.875 7.875 8.38125 7.875 9C7.875 9.61875 8.38125 10.125 9 10.125C9.61875 10.125 10.125 9.61875 10.125 9C10.125 8.38125 9.61875 7.875 9 7.875Z" />
                           </svg>
-                        </button>
+                        </button> */}
                         {/* Handle download */}
                         <button
                           onClick={() => handleDownload(packageItem)}
